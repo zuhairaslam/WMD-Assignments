fa4320093376d20982fc1b9fd6b6c4bca3a41a6e
Zuhair Aslam <45167703+zuhairaslam@users.noreply.github.com>
2023-02-01 17:59:13 +0500

    Add files via upload

---
+++ typescript/zuhair-guessgame/index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function StartFunc() {
+    const ComputerNumber = Math.floor(Math.random() * 10);
+    const PlayerNumber = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'userGuess',
+            message: "Enter Any Number from 1 to 10: "
+        }
+    ]);
+    const { userGuess } = PlayerNumber;
+    console.log("Your Guess: ", userGuess, "\nComputer's Guess: ", ComputerNumber);
+    if (userGuess === ComputerNumber) {
+        console.log("Congratulations!! You have correctly guessed it. \nYou Won ^_^ ");
+    }
+    else {
+        console.log("Ooopss!!!! Better luck next time!");
+    }
+}
+async function StarAgain() {
+    do {
+        await StartFunc();
+        var again = await inquirer
+            .prompt({
+            type: "input",
+            name: 'restart',
+            message: "Do you want to continue ? press Y or N"
+        });
+    } while (again.restart === "Y" || again.restart === "y" || again.restart === "YES" || again.restart === "yes");
+}
+StarAgain();
